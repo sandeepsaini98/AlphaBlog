ea6e6b2d4d791929993efbfb29fa6878308becdc
sandeepsaini98 <sandeepsaini85113@gmail.com>
2021-09-26 06:35:44 +0530

    Done till adding categories table

---
+++ test/integration/create_category_test.rb
@@ -0,0 +1,30 @@
+require "test_helper"
+
+setup do
+  @admin_user = User.create(username: "sainix", email: "sainix@example.com",password: "saini_9773", admin: true)
+  sign_in_as(@admin_user)
+end
+
+class CreateCategoryTest < ActionDispatch::IntegrationTest
+  test "get new category form and create category" do
+    get "/categories/new"
+    assert_response :success
+    assert_difference 'Category.count', 1 do
+      post categories_path, params: { category: { name: "Sports"} }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "Sports", response.body
+  end
+  test "get new category form and reject invalid category submission" do
+    get "/categories/new"
+    assert_response :success
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: " "} }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+end
