b8cb34866eca2cc3d0ab71b8ca695fcb41c592d4
sandeepsaini98 <sandeepsaini85113@gmail.com>
2021-09-27 23:14:46 +0530

    mms

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +11,7 @@ module AlphaBlog
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -20,5 +22,7 @@ module AlphaBlog
    
    # config.eager_load_paths << Rails.root.join("extras")
    config.autoloader = :classic
+   config.assets.initialize_on_precompile = false
   end
 end
+
